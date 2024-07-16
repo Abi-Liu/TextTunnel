cb96568aca6ab7c0d99accfa1bd496b265a475f6
Abi Liu <abiliu018@gmail.com>
2024-07-16 15:02:15 -0700

    restructure project

--- cmd/chat_server/server.go
+++ cmd/chat_server/server.go
@@ -2,30 +2,17 @@ package main
 
 import (
 	"log"
-	"net/http"
-	"time"
 
-	"github.com/Abi-Liu/TextTunnel/config"
 	"github.com/Abi-Liu/TextTunnel/internal/server"
 )
 
 func main() {
-	config, err := config.CreateConfig()
+	server, err := server.CreateServer()
 	if err != nil {
-		log.Fatalf("Error creating config: %v", err)
+		log.Fatalf("Error: %s", err)
 	}
 
-	r := server.NewRouter(config)
-
-	server := &http.Server{
-		Addr:              ":" + config.Env.PORT,
-		Handler:           r,
-		ReadTimeout:       10 * time.Second,
-		WriteTimeout:      10 * time.Second,
-		ReadHeaderTimeout: 10 * time.Second,
-	}
-
-	log.Printf("Starting server on port %s", config.Env.PORT)
+	log.Printf("Starting server on port %s", server.Addr)
 	if err := server.ListenAndServe(); err != nil {
 		log.Fatalf("Failed to start server: %v", err)
 	}
