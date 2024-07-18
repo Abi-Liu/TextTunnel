38c3144b5ab1cf87fb2ce097718d4b213564a41d
Abi Liu <abiliu018@gmail.com>
2024-07-17 18:52:02 -0700

    save token tests

--- internal/client/auth/auth_test.go
+++ internal/client/auth/auth_test.go
@@ -1,32 +1,70 @@
 package auth
 
-// import (
-// 	"fmt"
-// 	"os"
-// 	"testing"
-// )
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+	"testing"
+)
 
-// func TestGetConfigPath(t *testing.T) {
-// 	dir, err := os.UserHomeDir()
-// 	if err != nil {
-// 		t.FailNow()
-// 	}
-//
-// 	expectedString := fmt.Sprintf("%s/.texttunnel/config.json", dir)
-// 	actualString, actualErr := getConfigPath()
-//
-// 	if expectedString != actualString {
-// 		t.Fatalf("Expected %s, Received %s", expectedString, actualString)
-// 	}
-//
-// 	if actualErr != nil {
-// 		t.Fatalf("Expected nil error, received %s", actualErr)
-// 	}
-// }
-//
-// func TestSaveToken(t *testing.T) {
-// 	err := SaveToken("TestToken")
-// 	if err != nil {
-// 		t.Fatalf("Expected nil, received err: %s", err)
-// 	}
-// }
+type MockFileSystem struct {
+	files map[string][]byte
+	home  string
+}
+
+const HOME_DIR = "test/home"
+
+func NewMockFileSystem() *MockFileSystem {
+	return &MockFileSystem{files: make(map[string][]byte), home: HOME_DIR}
+}
+
+func (m *MockFileSystem) ReadFile(filename string) ([]byte, error) {
+	data, exists := m.files[filename]
+	if !exists {
+		return nil, errors.New("file not found")
+	}
+	return data, nil
+}
+
+func (m *MockFileSystem) WriteFile(filename string, data []byte, perm os.FileMode) error {
+	m.files[filename] = data
+	return nil
+}
+
+func (m *MockFileSystem) UserHomeDir() (string, error) {
+	return m.home, nil
+}
+
+func (m *MockFileSystem) MkDirAll(path string, perm os.FileMode) error {
+	return nil
+}
+
+func TestSaveToken(t *testing.T) {
+	fs := NewMockFileSystem()
+	cm := ConfigManager{FS: fs}
+	err := cm.SaveToken("Test")
+
+	if err != nil {
+		t.Fatalf("Expected nil error. Received %s", err)
+	}
+
+	expectedPath := fmt.Sprintf("%s/.texttunnel/config.json", HOME_DIR)
+	data, err := fs.ReadFile(expectedPath)
+	if err != nil {
+		t.Fatalf("Encountered error when reading from file: %s", err)
+	}
+
+	file := make(map[string]string)
+	err = json.Unmarshal(data, &file)
+
+	if err != nil {
+		t.Fatalf("Failed to unmarshal json")
+	}
+
+	token, _ := file[TOKEN_KEY]
+
+	if token != "Test" {
+		t.Fatalf("Expected Test, Received: %s", string(data))
+	}
+}
