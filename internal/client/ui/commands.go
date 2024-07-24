f2d7f3b5671664979c765074fa128299c8cd59a0
Abi Liu <abiliu018@gmail.com>
2024-07-24 15:15:15 -0700

    websocket tea c ommands

--- internal/client/ui/commands.go
+++ internal/client/ui/commands.go
@@ -7,10 +7,13 @@ import (
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/google/uuid"
 	"nhooyr.io/websocket"
+	"nhooyr.io/websocket/wsjson"
 )
 
 type wsConnectionMsg struct {
-	conn *websocket.Conn
+	conn   *websocket.Conn
+	ctx    context.Context
+	cancel context.CancelFunc
 }
 
 type connectionErrorMsg struct {
@@ -23,11 +26,48 @@ func connectToRoom(id uuid.UUID) tea.Cmd {
 		header.Set("Authorization", "Bearer "+httpClient.AuthToken)
 		dialOpts := websocket.DialOptions{HTTPHeader: header}
 
-		c, _, err := websocket.Dial(context.Background(), "ws://localhost:8080/ws/"+id.String(), &dialOpts)
+		ctx, cancel := context.WithCancel(context.Background())
+		c, _, err := websocket.Dial(ctx, "ws://localhost:8080/ws/"+id.String(), &dialOpts)
+		if err != nil {
+			cancel()
+			return connectionErrorMsg{err: err}
+		}
+
+		return wsConnectionMsg{conn: c, ctx: ctx, cancel: cancel}
+	}
+}
+
+type receiveMsg struct {
+	msg message
+}
+
+func read(conn *websocket.Conn, ctx context.Context) tea.Cmd {
+	return func() tea.Msg {
+		msg := message{}
+		err := wsjson.Read(ctx, conn, &msg)
+		if err != nil {
+			return connectionErrorMsg{err: err}
+		}
+		return receiveMsg{msg: msg}
+	}
+}
+
+func write(conn *websocket.Conn, ctx context.Context, userId, roomId uuid.UUID, msg string) tea.Cmd {
+	return func() tea.Msg {
+		type params struct {
+			SenderId uuid.UUID `json:"sender_id"`
+			RoomId   uuid.UUID `json:"room_id"`
+			Content  string    `json:"content"`
+		}
+		err := wsjson.Write(ctx, conn, params{
+			SenderId: userId,
+			RoomId:   roomId,
+			Content:  msg,
+		})
 		if err != nil {
 			return connectionErrorMsg{err: err}
 		}
 
-		return wsConnectionMsg{conn: c}
+		return nil
 	}
 }
