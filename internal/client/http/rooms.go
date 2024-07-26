daf5ba935aa6e8e4bc9402b6bb01858842d8cd9d
Abi Liu <abiliu018@gmail.com>
2024-07-25 23:36:14 -0700

    create rooms http method

--- internal/client/http/rooms.go
+++ internal/client/http/rooms.go
@@ -1,7 +1,9 @@
 package http
 
 import (
+	"bytes"
 	"encoding/json"
+	"fmt"
 	"io"
 )
 
@@ -23,4 +25,33 @@ func (c HttpClient) FetchRooms() ([]Room, error) {
 	}
 
 	return rooms, nil
+}
+
+func (c HttpClient) CreateRoom(name string) (Room, error) {
+	type Req struct {
+		Name string `json:"name"`
+	}
+	params := Req{Name: name}
+	data, err := json.Marshal(params)
+	if err != nil {
+		return Room{}, fmt.Errorf("Failed to Marshal to json: %s", err)
+	}
+	reader := bytes.NewReader(data)
+	res, err := c.Post(BASE_URL+"/rooms", reader)
+	if err != nil {
+		return Room{}, fmt.Errorf("Failed to create room: %s", err)
+	}
+	defer res.Body.Close()
+
+	dat, err := io.ReadAll(res.Body)
+	if err != nil {
+		return Room{}, fmt.Errorf("Failed to read response body: %s", err)
+	}
+	room := Room{}
+	err = json.Unmarshal(dat, &room)
+	if err != nil {
+		return Room{}, fmt.Errorf("Failed to Unmarshal data: %s", err)
+	}
+
+	return room, nil
 }
