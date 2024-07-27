72edd6e138d3fa6abcf8bed3f78b351a4d9ec707
Abi Liu <abiliu018@gmail.com>
2024-07-26 20:32:56 -0700

    tests for respond with json

--- go.mod
+++ go.mod
@@ -20,6 +20,7 @@ require (
 	github.com/charmbracelet/x/input v0.1.0 // indirect
 	github.com/charmbracelet/x/term v0.1.1 // indirect
 	github.com/charmbracelet/x/windows v0.1.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/erikgeiser/coninput v0.0.0-20211004153227-1c3628e74d0f // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.18 // indirect
@@ -29,10 +30,13 @@ require (
 	github.com/muesli/cancelreader v0.2.2 // indirect
 	github.com/muesli/reflow v0.3.0 // indirect
 	github.com/muesli/termenv v0.15.2 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/sahilm/fuzzy v0.1.1-0.20230530133925-c48e322e2a8f // indirect
+	github.com/stretchr/testify v1.9.0 // indirect
 	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
 	golang.org/x/sync v0.7.0 // indirect
 	golang.org/x/sys v0.22.0 // indirect
 	golang.org/x/text v0.16.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ internal/server/json_helper_tests.go
@@ -0,0 +1,46 @@
+package server
+
+import (
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRespondWithJson(t *testing.T) {
+	tests := map[string]struct {
+		statusCode         int
+		expectedStatusCode int
+		payload            interface{}
+		expectedBody       string
+	}{
+		"valid payload": {
+			expectedStatusCode: 200,
+			statusCode:         200,
+			payload:            map[string]string{"message": "Hello World"},
+			expectedBody:       `"message": "Hello World"`,
+		},
+		"empty payload": {
+			expectedStatusCode: 201,
+			statusCode:         201,
+			payload:            map[string]string{},
+			expectedBody:       `{}`,
+		},
+		"null payload": {
+			expectedStatusCode: 204,
+			statusCode:         204,
+			payload:            nil,
+			expectedBody:       "null",
+		},
+	}
+
+	for name, test := range tests {
+		t.Logf("Running test: %s", name)
+		rr := httptest.NewRecorder()
+		RespondWithJson(rr, test.statusCode, test.payload)
+
+		assert.Equal(t, test.expectedStatusCode, rr.Code)
+		assert.Equal(t, "application/json", rr.Header().Get("Content-Type"))
+		assert.Equal(t, test.expectedBody, rr.Body.String())
+	}
+}
